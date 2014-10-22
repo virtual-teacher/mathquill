99a13a7b3da5eaa66870b392d9b2beda91eb1cb8
Joel Burget <joel@khanacademy.org>
2014-10-22 16:48:23 -0700

    tmp

--- src/commands/unit/commands.js
+++ src/commands/unit/commands.js
@@ -2,64 +2,52 @@
  * Commands and Operators.
  **************************/
 
-var UnitSupSub = P(UnitCommand, function(_, super_) {
-  _.createLeftOf = function(cursor) {
-    if (!cursor[L] && cursor.options.supSubsRequireOperand) return;
-    return super_.createLeftOf.apply(this, arguments);
-  };
+function classesAmongAncestors(cursor, classes) {
+    while (cursor.parent) {
+        // we can't nest fractions within fractions or superscripts
+        for (var i = 0; i < classes.length; i++) {
+            if (cursor instanceof classes[i]) {
+                return true;
+            }
+        }
+        cursor = cursor.parent;
+    }
+    return false;
+}
 
-  Options.p.charsThatBreakOutOfSupSub = '';
-
-  _.finalizeTree = function() {
-    this.ends[L].write = function(cursor, ch) {
-      // Break out of super/subscripts if character matches,
-      // unless it's the negative sign at the very start.
-      if (cursor.options.charsThatBreakOutOfSupSub.indexOf(ch) > -1 &&
-          !(ch === '-' && !cursor[L]) ) {
-        cursor.insRightOf(this.parent);
-      }
-      UnitBlock.p.write.apply(this, arguments);
-    };
-  };
-
-  _.addBlock = function(block) {
-    this.sub = this.downInto = this.sup.downOutOf = block;
-    block.adopt(this, 0, this.sup).upOutOf = this.sup;
-    block.jQ = $('<span class="mq-sub"></span>').append(block.jQ.children())
-      .attr(mqBlockId, block.id).appendTo(this.jQ.removeClass('mq-sup-only'));
-    this.jQ.append('<span style="display:inline-block;width:0">&nbsp;</span>');
-
-    cmd.sup.deleteOutOf = function(dir, cursor) {
-        debugger;
-      cursor.insDirOf(dir, this.parent);
-      if (!this.isEmpty()) {
-        cursor[-dir] = this.ends[dir];
-        this.children().disown()
-          .withDirAdopt(dir, cursor.parent, cursor[dir], this.parent)
-          .jQ.insDirOf(dir, this.parent.jQ);
-      }
-      cmd.supsub = "sub";
-      delete cmd.sup;
-      delete cmd.upInto;
-      cmd.sub.upOutOf = insLeftOfMeUnlessAtEnd;
-      delete cmd.sub.deleteOutOf;
-      this.remove();
-    };
-  };
-});
-
-UnitCmds['^'] = P(UnitSupSub, function(_, super_) {
+var UnitSup =
+UnitCmds["^"] = P(UnitCommand, function(_, super_) {
   _.supsub = 'sup';
+
   _.htmlTemplate =
       '<span class="mq-supsub mq-non-leaf mq-sup-only">'
     +   '<span class="mq-sup">&0</span>'
     + '</span>'
   ;
+
   _.textTemplate = [ '^' ];
+
+  _.createLeftOf = function(cursor) {
+    // don't create the superscript if there's nothing to the left of the
+    // cursor or we're within a superscript
+    if (!cursor[L] || classesAmongAncestors(cursor, [UnitSup])) {
+        return;
+    }
+
+    return super_.createLeftOf.apply(this, arguments);
+  };
+
   _.finalizeTree = function() {
     this.upInto = this.sup = this.ends[R];
     this.sup.downOutOf = insLeftOfMeUnlessAtEnd;
-    super_.finalizeTree.call(this);
+
+    this.ends[L].write = function(cursor, ch) {
+      // Break out of super/subscripts on space
+      if (ch === " ") {
+        cursor.insRightOf(this.parent);
+      }
+      UnitBlock.p.write.apply(this, arguments);
+    };
   };
 });
 
@@ -81,12 +69,9 @@ UnitCmds['/'] = P(UnitCommand, function(_, super_) {
   };
 
   _.createLeftOf = function(cursor) {
-    var curseeeeeees = cursor;
-    while (curseeeeeees.parent) {
-        if (curseeeeeees instanceof UnitLiveFraction) {
-            return;
-        }
-        curseeeeeees = curseeeeeees.parent;
+    // we can't nest fractions within fractions or superscripts
+    if (classesAmongAncestors(cursor, [UnitLiveFraction, UnitSup])) {
+        return;
     }
 
     if (!this.replacedFragment) {
